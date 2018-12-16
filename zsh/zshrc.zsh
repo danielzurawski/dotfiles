7ae5c782c2fa5affd1451f9165037a20bc8edb2a
Daniel Zurawski <zurawski.daniel@gmail.com>
2018-12-16 18:54:43 +0000

    feat(): initial commit of new dotfiles

---
+++ zsh/zshrc.zsh
@@ -0,0 +1,27 @@
+HISTFILE=~/.histfile
+HISTSIZE=100000
+SAVEHIST=100000
+
+setopt appendhistory autocd extendedglob nomatch notify
+unsetopt beep
+
+zstyle :compinstall filename '~/.zshrc'
+
+autoload -Uz compinit
+compinit
+
+# source zpresto
+source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
+# source the blox prompt + customizations
+source "${ZDOTDIR:-$HOME}/.config/zsh/prompt.zsh"
+# https://github.com/zsh-users/zsh-autosuggestions
+source "${ZDOTDIR:-$HOME}/.config/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"
+# source nvm as a zsh plugin (https://github.com/lukechilds/zsh-nvm)
+source "${ZDOTDIR:-$HOME}/.config/zsh/zsh-nvm/zsh-nvm.plugin.zsh"
+
+# custom config
+source "${ZDOTDIR:-$HOME}/.config/zsh/aliases.zsh"
+source "${ZDOTDIR:-$HOME}/.config/zsh/functions.zsh"
+source "${ZDOTDIR:-$HOME}/.config/zsh/keybindings.zsh"
+source "${ZDOTDIR:-$HOME}/.config/zsh/git.zsh"
+source "${ZDOTDIR:-$HOME}/.config/zsh/docker.zsh"
